d195ff1b5ee0c19b2782028aa4be7a02fe5604ef
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-04-01 19:52:13 +0400

    layout component setup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,22 @@
 import { Routes } from '@angular/router';
 import { HomePageComponent } from './Components/home-page/home-page.component';
 import { LayoutComponent } from './Components/layout/layout.component';
+import { HeroComponent } from './Components/hero/hero.component';
 
 export const routes: Routes = [
     {
         path: "",
+        component:HeroComponent,
+        pathMatch: 'full'
+    },
+    {
+        path: "home",
         component: LayoutComponent,
         children: [
-            { path: "", component: HomePageComponent }
+            { 
+                path: "", 
+                component: HomePageComponent 
+            }
         ]
     }
 ];
