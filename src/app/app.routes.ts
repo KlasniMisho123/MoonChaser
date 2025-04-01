a291ef321835529ae97916edc23de7c67d966468
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-04-01 19:47:55 +0400

    layout component setup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,8 @@
-import { Routes, RouterModule } from '@angular/router';
+import { Routes } from '@angular/router';
 import { HomePageComponent } from './Components/home-page/home-page.component';
 import { LayoutComponent } from './Components/layout/layout.component';
-const routes: Routes = [
+
+export const routes: Routes = [
     {
         path: "",
         component: LayoutComponent,
