312b3daf6209e776a705b51cf278973721a9b2e3
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-04-02 15:44:47 +0400

    theme change heropage

--- src/app/Components/hero/hero.component.ts
+++ src/app/Components/hero/hero.component.ts
@@ -1,9 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { Component, Renderer2 } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-hero',
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './hero.component.html',
   styleUrl: './hero.component.css'
 })
