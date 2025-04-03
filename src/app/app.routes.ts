585282b8e12939ae973f6d89413c3262245fe7fa
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-04-03 22:36:27 +0400

    navbar commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,8 @@ import { HomePageComponent } from './Components/home-page/home-page.component';
 import { LayoutComponent } from './Components/layout/layout.component';
 import { HeroComponent } from './Components/hero/hero.component';
 import { WeatherComponent } from './Components/weather/weather.component';
+import { MoonComponent } from './Components/moon/moon.component';
+import { AboutusComponent } from './Components/aboutus/aboutus.component';
 
 export const routes: Routes = [
     {
@@ -27,6 +29,26 @@ export const routes: Routes = [
             {
                 path: "", 
                 component: WeatherComponent
+            }
+        ]
+    },
+    {
+        path: "moon",
+        component: LayoutComponent, 
+        children: [
+            {
+                path: "", 
+                component: MoonComponent
+            }
+        ]
+    },
+    {
+        path: "aboutus",
+        component: LayoutComponent, 
+        children: [
+            {
+                path: "", 
+                component: AboutusComponent
             }
         ]
     }
