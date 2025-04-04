ffbf346cb09338da9452e1f0ece6804c6e69eee1
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-04-04 22:32:37 +0400

    responsive commit

--- src/app/Components/daily-weather/daily-weather.component.ts
+++ src/app/Components/daily-weather/daily-weather.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-daily-weather',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './daily-weather.component.html',
   styleUrl: './daily-weather.component.css'
 })
 export class DailyWeatherComponent {
+  inputedCity: string = ""
 
+  getInputedCity(city:string):void {
+    this.inputedCity = city
+    console.log(city)
+  }
 }
