1c0ed472db67f90bd8843928d1b35f2c6a1bb3b8
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-04-04 17:45:07 +0400

    weather daily/weekly commit

--- src/app/Components/weather/weather.component.ts
+++ src/app/Components/weather/weather.component.ts
@@ -1,11 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-weather',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './weather.component.html',
   styleUrl: './weather.component.css'
 })
 export class WeatherComponent {
+
+  currentWeatherSection: string = "Daily"
+
+  setWeatherSection(section:string): void {
+    this.currentWeatherSection = section
+  }
 
 }
