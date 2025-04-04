2da5b9a6f8cff8c80bcfe35ef552ba5bc9ba98c3
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-04-04 17:54:18 +0400

    weather daily/weekly commit

---
+++ src/app/Components/weekly-weather/weekly-weather.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-weekly-weather',
+  imports: [],
+  templateUrl: './weekly-weather.component.html',
+  styleUrl: './weekly-weather.component.css'
+})
+export class WeeklyWeatherComponent {
+
+}
