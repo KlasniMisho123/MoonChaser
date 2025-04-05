86645821ebfc079becbc79412ed48c54d99cd44b
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-04-05 12:34:06 +0400

    API INTEGRATION COMMIT

--- src/app/Services/daily-weather.service.ts
+++ src/app/Services/daily-weather.service.ts
@@ -1,9 +1,16 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
+// API KEY:92c4065289c84a2cb1982555250504
 @Injectable({
   providedIn: 'root'
 })
 export class DailyWeatherService {
 
-  constructor() { }
+  http = inject(HttpClient)
+
+  getDailyWeather():Observable<any> {
+    return this.http.get<any>("http://api.weatherapi.com/v1/current.json?key=92c4065289c84a2cb1982555250504&q=London&aqi=no")
+  }
 }
