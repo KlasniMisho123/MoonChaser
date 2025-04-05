343c33a7cc16b315c19890771831a8b7faae17e4
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-04-05 20:49:03 +0400

    secret API_KEY commit

--- src/environments/environments.service.ts
+++ src/environments/environments.service.ts
@@ -1,4 +1,10 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const weather_api_key = process.env['WEATHER_API_KEY'];
+
 export const environment = {
-  CURRENT_WEATHER_API_URL_BASE: "http://api.weatherapi.com/v1/current.json?key=92c4065289c84a2cb1982555250504&q=",
-  FORECAST_WEATHER_API_URL_BASE: "http://api.weatherapi.com/v1/forecast.json?key=92c4065289c84a2cb1982555250504&q="
+  CURRENT_WEATHER_API_URL_BASE: `http://api.weatherapi.com/v1/current.json?key=${weather_api_key}&q=`,
+  FORECAST_WEATHER_API_URL_BASE: `http://api.weatherapi.com/v1/forecast.json?key=${weather_api_key}&q=`
 }
