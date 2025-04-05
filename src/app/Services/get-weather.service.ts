8e0bb5fbb80a52fb35b611a8b8fca5e024cf93ca
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-04-05 12:44:37 +0400

    dummy api call

--- src/app/Services/get-weather.service.ts
+++ src/app/Services/get-weather.service.ts
@@ -11,8 +11,8 @@ export class GetWeatherService {
   http = inject(HttpClient)
 
   getDailyWeather(cityName: string):Observable<any> {
-    // return this.http.get<any>("http://api.weatherapi.com/v1/current.json?key=92c4065289c84a2cb1982555250504&q=London&aqi=no")
-    return this.http.get<any>("http://api.weatherapi.com/v1/current.json?" + "key" + cityName )
+    return this.http.get<any>("http://api.weatherapi.com/v1/current.json?key=92c4065289c84a2cb1982555250504&q=London&aqi=no")
+    // return this.http.get<any>("http://api.weatherapi.com/v1/current.json?" + "key" + cityName )
   }
 
   getWeeklyWeather():Observable<any> {
