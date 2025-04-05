68e468a7f53f8d9f8aed042b245b9c31bb131f22
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-04-05 12:39:15 +0400

    API INTEGRATION COMMIT

--- src/app/Services/get-weather.service.ts
+++ src/app/Services/get-weather.service.ts
@@ -1,9 +1,20 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
+// API KEY:92c4065289c84a2cb1982555250504
 @Injectable({
   providedIn: 'root'
 })
 export class GetWeatherService {
 
-  constructor() { }
+  http = inject(HttpClient)
+
+  getDailyWeather():Observable<any> {
+    return this.http.get<any>("http://api.weatherapi.com/v1/current.json?key=92c4065289c84a2cb1982555250504&q=London&aqi=no")
+  }
+
+  getWeeklyWeather():Observable<any> {
+    return<any> "Weekly Weather Dummy Text"
+  }
 }
