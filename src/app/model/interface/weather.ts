ea70d87c0d9298fd1ccdaf98a032f97bb32fade0
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-04-05 19:11:18 +0400

    daily weather interface

--- src/app/model/interface/weather.ts
+++ src/app/model/interface/weather.ts
@@ -14,10 +14,18 @@ export interface SimpleWeatherResponse {
     location: {
       name: string;
       country: string;
+      localtime: string;
     };
     current: {
       temp_c: number;
       humidity: number;
+      condition: {
+         text: string,
+         icon: string,
+      },
+      cloud: number,
+      feelslike_c: number,
+      windchill_c: number
     };
   }
   
