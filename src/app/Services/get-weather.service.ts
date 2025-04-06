c4a8f774e870d5a94befc486d08bce567cbbfc98
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-04-06 22:49:59 +0400

    weekly interface commit

--- src/app/Services/get-weather.service.ts
+++ src/app/Services/get-weather.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { SimpleWeatherResponse } from '../model/interface/weather';
+import { SevenDaysForecast, SimpleWeatherResponse } from '../model/interface/weather';
 import { environment } from '../../environments/environments.service';
 
 @Injectable({
@@ -15,8 +15,7 @@ export class GetWeatherService {
     return this.http.get<SimpleWeatherResponse>(environment.CURRENT_WEATHER_API_URL_BASE + `${cityName}&aqi=no`)
   }
   
-  getWeeklyWeather(cityName: string):Observable<any> {
-    return this.http.get<any>(environment.FORECAST_WEATHER_API_URL_BASE + `${cityName}&days=7&aqi=no&alerts=no`)
+  getWeeklyWeather(cityName: string):Observable<SevenDaysForecast> {
+    return this.http.get<SevenDaysForecast>(environment.FORECAST_WEATHER_API_URL_BASE + `${cityName}&days=7&aqi=no&alerts=no`)
   }
-  // Weekly API RESPONSE MODEL
 }
--- src/app/model/interface/weather.ts
+++ src/app/model/interface/weather.ts
@@ -1,7 +1,4 @@
-export interface weatherAPIresponse {
-    
-}
-  
+
 export interface SimpleWeatherResponse {
     location: {
       name: string;
@@ -30,6 +27,28 @@ export interface SimpleWeatherResponse {
   
   export interface SevenDaysForecast extends SimpleWeatherResponse {
     forecast: {
-      
+      forecast: [
+        {
+          date: string,
+          day: {
+            temp_c: number;
+            temp_f: number,
+            humidity: number;
+            condition: {
+              text: string,
+              icon: string,
+           },
+          precip_mm: number,
+          precip_in: number,
+          wind_kph: number,
+          wind_mph: number,
+          wind_degree: number,
+          wind_dir: string,
+          cloud: number,
+          feelslike_c: number,
+          windchill_c: number
+          },
+        }
+      ]
     }
   }
