362124c53cc0786a4e6d6a42cb06c79f8b0f6719
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-04-12 19:54:32 +0400

    DatePipe Formating

--- src/app/Components/moon/moon.component.ts
+++ src/app/Components/moon/moon.component.ts
@@ -1,21 +1,19 @@
 import { Component,  inject,  OnInit } from '@angular/core';
 import { GetWeatherService } from '../../Services/get-weather.service';
-import { DatePipe } from '@angular/common';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-moon',
-  imports: [DatePipe],
+  imports: [CommonModule],
   templateUrl: './moon.component.html',
   styleUrl: './moon.component.css'
 })
 export class MoonComponent implements OnInit{
 
 weatherService = inject(GetWeatherService)
-datePipe = inject(DatePipe)
 
 currentMoonPhase: string = "";
 
-currentDate:string= ""
 rawDate!:Date
 
   ngOnInit(): void {
@@ -26,9 +24,8 @@ rawDate!:Date
 
   getCurrentMoonPhase() {
     this.rawDate = new Date()
-    this.currentDate = this.datePipe.transform(this.rawDate, 'yyyy-MM-dd') || '';
 
-    console.log("currentDate: ", this.currentDate)
+    console.log("currentDate: ", this.rawDate)
     this.weatherService.getAstronomyInfo("tbilisi","2025-04-12").subscribe({
       next: (res:any) => {
         console.log("res: ", res)
