d3a706289456c7a242da2d13e6a5fd9110e66d09
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-04-12 15:12:13 +0400

    minor commit

--- src/environments/environments.service.ts
+++ src/environments/environments.service.ts
@@ -3,10 +3,10 @@
 // dotenv.config();
 
 // const weather_api_key = process.env['WEATHER_API_KEY'];
-
+const weather_api_key = '92c4065289c84a2cb1982555250504'
 export const environment = {
   // CURRENT_WEATHER_API_URL_BASE: `http://api.weatherapi.com/v1/current.json?key=${weather_api_key}&q=`,
   // FORECAST_WEATHER_API_URL_BASE: `http://api.weatherapi.com/v1/forecast.json?key=${weather_api_key}&q=`
-  CURRENT_WEATHER_API_URL_BASE: `http://api.weatherapi.com/v1/current.json?key=92c4065289c84a2cb1982555250504&q=`,
-  FORECAST_WEATHER_API_URL_BASE: `http://api.weatherapi.com/v1/forecast.json?key=92c4065289c84a2cb1982555250504&q=`
+  CURRENT_WEATHER_API_URL_BASE: `http://api.weatherapi.com/v1/current.json?key=${weather_api_key}&q=`,
+  FORECAST_WEATHER_API_URL_BASE: `http://api.weatherapi.com/v1/forecast.json?key=${weather_api_key}&q=`
 }
