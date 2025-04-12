5b1ad14dad01bcc321588ffcd75d845850d68871
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-04-12 22:55:45 +0400

    rotate with moonphase

--- src/app/Components/moon/moon.component.ts
+++ src/app/Components/moon/moon.component.ts
@@ -76,40 +76,40 @@ ngOnInit(): void {
       case 1:
         // code for waxing crescent
         // this.activeStyle = "Waxing Crescent";
-        this.activeStyle = "Waxing Crescent";
+        this.activeStyle = "rotate-[-135deg]";
         break;
       case 2:
         // code for first quarter
         // this.activeStyle = "First Quarter";
-        this.activeStyle = "First Quarter";
+        this.activeStyle = "rotate-[-180deg]";
         break;
       case 3:
         // code for waxing gibbous
         // this.activeStyle = "Waxing Gibbous";
-        this.activeStyle = "Waxing Gibbous";
+        this.activeStyle = "rotate-[-225deg]";
         break;
       case 4:
         // code for full moon
         // this.activeStyle = "Full Moon";
-        this.activeStyle = "Full Moon";
+        this.activeStyle = "rotate-[-270deg]";
         break;
       case 5:
         // code for waning gibbous
         // this.activeStyle = "Waning Gibbous";
-        this.activeStyle = "Waning Gibbous";
+        this.activeStyle = "rotate-[-315deg]";
         break;
       case 6:
         // code for last quarter
         // this.activeStyle = "Last Quarter";
-        this.activeStyle = "Last Quarter";
+        this.activeStyle = "rotate-[-360deg]";
         break;
       case 7:
         // code for waning crescent
         // this.activeStyle = "Waning Crescent";
-        this.activeStyle = "Waning Crescent";
+        this.activeStyle = "rotate-[-405deg]";
         break;
       default:
-        this.activeStyle = ""
+        this.activeStyle = "rotate-[-90deg]"
         break;
     }
   }
