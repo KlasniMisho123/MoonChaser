c3361423c5452e177e666e6773a005aa3ad0121a
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-04-12 22:31:51 +0400

    rotate with moonphase

--- src/app/Components/moon/moon.component.ts
+++ src/app/Components/moon/moon.component.ts
@@ -43,21 +43,62 @@ ngOnInit(): void {
   // this.getCurrentMoonPhase(this.selectedCity)
 }
 
-targetMoonPhaseCard(cardIndex:number) {
-if(cardIndex === this.targetedMoonPhase) {
-  this.isMoonPhaseSelected = false
-  this.targetedMoonPhase = -1;
-  this.selecteMoonPhaseInfo = {};
-} else {
-  this.isMoonPhaseSelected = true;
-  this.targetedMoonPhase = cardIndex;
-  // this.selectedMoonPhaseInfo = {}; set INFO
-}
-}
+  targetMoonPhaseCard(cardIndex:number) {
+    if(cardIndex === this.targetedMoonPhase) {
+    this.isMoonPhaseSelected = false
+    this.targetedMoonPhase = -1;
+    this.selecteMoonPhaseInfo = {};
+    } else {
+      this.isMoonPhaseSelected = true;
+      this.targetedMoonPhase = cardIndex;
+      this.setMoonPhaseInfo(cardIndex)
+    }
+  }
 
   setMoonPhaseInfo(cardIndex:number):void {
     this.selectedMoonPhaseInfo = moonPhaseInfo[cardIndex];
   }
+
+  rotateWithMoonPhase(cardIndex: number) {
+    switch (cardIndex) {
+      case 0:
+        // code for new moon
+        console.log("New Moon");
+        break;
+      case 1:
+        // code for waxing crescent
+        console.log("Waxing Crescent");
+        break;
+      case 2:
+        // code for first quarter
+        console.log("First Quarter");
+        break;
+      case 3:
+        // code for waxing gibbous
+        console.log("Waxing Gibbous");
+        break;
+      case 4:
+        // code for full moon
+        console.log("Full Moon");
+        break;
+      case 5:
+        // code for waning gibbous
+        console.log("Waning Gibbous");
+        break;
+      case 6:
+        // code for last quarter
+        console.log("Last Quarter");
+        break;
+      case 7:
+        // code for waning crescent
+        console.log("Waning Crescent");
+        break;
+      default:
+        console.log("Unknown phase");
+        break;
+    }
+  }
+  
 
   getCurrentMoonPhase(inputedCity:string) {
     this.rawDate = new Date();
