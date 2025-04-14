659cc8b250bce17c3ad13f01a34a97e4d1e2382a
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-04-14 19:49:33 +0400

    currentmoonphase interface

--- src/app/Components/header/header.component.ts
+++ src/app/Components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Renderer2 } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
@@ -8,5 +8,19 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+  isNight: boolean = true;
 
+  constructor(private renderer: Renderer2) {}
+
+  toggleDayNight():void {
+    this.isNight = !this.isNight
+
+    if(this.isNight) {
+      this.renderer.addClass(document.body, 'night-mode');
+      this.renderer.removeClass(document.body, 'day-mode');
+    } else {
+      this.renderer.addClass(document.body, 'day-mode');
+      this.renderer.removeClass(document.body, 'night-mode');
+    }
+  }
 }
